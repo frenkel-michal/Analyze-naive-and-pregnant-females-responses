c5db4e7893a2eda90d02ccce90d2ad5a9f7b87d5
frenkel-michal <52693404+frenkel-michal@users.noreply.github.com>
2019-07-09 11:44:05 +0300

    Update plot_units_for_manual_selection.m

--- plot_units_for_manual_selection.m
+++ plot_units_for_manual_selection.m
@@ -1,6 +1,8 @@
 function plot_units_for_manual_selection(PVAL,PTHRESH,DATA_STRUCT,TAKE_EPOCH,TRIALS,UNIT_ORDER,do_stims,basepath)
 % the function go over all the significant responding units and plot the raster and psth of the response
 % in addition the score of the response and the response time is saved in the excel file
+
+%% find the significant responding units data: 
 SIGS     = PVAL <= PTHRESH;
 anysigs         = sum(SIGS,2) > 0;  % any significant response
 UNIT_NUMS        = DATA_STRUCT.UNIT_NUMS;
@@ -11,7 +13,6 @@ units     = UNIT_NUMS(anysigs);
 dates     = SESSION_DATES(anysigs);
 sites     = SESSION_SITES(anysigs);
 RelTrials = TRIALS(anysigs);
-% sig_units=SIGS(anysigs,:);
 pval_units=PVAL(anysigs,:);
 do_odors=1:size(SIGS,2);
 switch TAKE_EPOCH
@@ -23,6 +24,7 @@ switch TAKE_EPOCH
         RelCuts=2;
 end
 
+%% ploting the raster and psth and changing the excel table
 % [num,~,RAW]=xlsread([basepath,'\huji_cell_database']);
 [num,~,RAW]=xlsread(['H:\tmp\tmp.xlsx']);
 headers     = RAW(1,1:end);
@@ -38,12 +40,12 @@ resp_letter = alphabet(resp_col);
 user_letter = alphabet(user_col);
 units_list  = cell2mat(RAW(2:end,unit_col));
 sites_list  = cell2mat(RAW(2:end,site_col));
-for ui=1:length(units)
-    units_row=find(units_list==units(ui))+1;%num is without the headers
+for ui=1:length(units) %gonig over the units
+    units_row=find(units_list==units(ui))+1;% unit_list is without the headers
     dates_row=find(strcmp(RAW(:,date_col),dates(ui)));
-    site_row=find(sites_list==sites(ui))+1;
+    site_row=find(sites_list==sites(ui))+1;% sites_list is without the headers
     rel_row=intersect(intersect(units_row,dates_row),site_row);
-    if isnan(RAW{rel_row,comm_col})
+    if isnan(RAW{rel_row,comm_col}) % if the unit is not graded yet, plot it and grade it
     [user_comm,resp,comm]= plot_unit(ui,unit_list,RelTrials,do_odors,RelCuts,dates,sites,units,pval_units,do_stims);
     if size(rel_row)==1
         xlswrite(['H:\tmp\tmp.xlsx'],comm.String(comm.Value),1,[comm_letter, num2str(rel_row)])
